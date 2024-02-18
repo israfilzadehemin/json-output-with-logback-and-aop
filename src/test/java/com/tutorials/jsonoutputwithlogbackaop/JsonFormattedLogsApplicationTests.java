893f37f3b10cc5633a7a13fc945665c4821010e9
eminisra <israfilzadehemin@gmailcom>
2024-02-18 12:32:18 +0100

    Initial implementation

---
+++ src/test/java/com/tutorials/jsonoutputwithlogbackaop/JsonFormattedLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.jsonoutputwithlogbackaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonFormattedLogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
